ee5f51d9f875a626df3f4814b47850dc53c467c6
Omar ayesh <165605860+Omar-1000@users.noreply.github.com>
2026-07-12 23:19:09 +0300

    Change email credentials in app configuration
    Updated email configuration with new credentials.

--- app.py
+++ app.py
@@ -81,9 +81,9 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_TLS"] = False
 app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = "serverehealth@gmail.com"
-app.config["MAIL_PASSWORD"] = "kknd ptcb buyj derl"
-app.config["MAIL_DEFAULT_SENDER"] = "serverehealth@gmail.com"
+app.config["MAIL_USERNAME"] = "serverehealth@example.com"
+app.config["MAIL_PASSWORD"] = " your key password "
+app.config["MAIL_DEFAULT_SENDER"] = "serverehealth@example.com"
 
 
 mail = Mail(app)
